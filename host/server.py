932a6fdaf12563305db86b3884af5e001190917f
Sssky-18 <ntytianyuan@gmail.com>
2023-12-03 13:15:12 -0800

    update

--- host/server.py
+++ host/server.py
@@ -1,21 +1,99 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import json
+import scipy
+import numpy as np
+from scipy.optimize import fsolve
+import datetime
+from math import sqrt
+import time
 
 app = Flask(__name__)
-received_strings = []
+COORD = [[-3,0], [3,1], [0,4]]
+V = 1503.84
+
+def tdoa(coord, v, delta_t):
+    result = []
+    # microphones
+    m0 = coord[0]  # coordinates of mic 0 (x,y)
+    m1 = coord[1]  # coordinates of mic 1 (x,y)
+    m2 = coord[2]  # coordinates of mic 2 (x,y)
+
+    v = v  # velocity of sound in saltwater at 12 degrees C
+
+    # tdoa
+    t1 = delta_t[0]  # tdoa between mic 0 and 1
+    t2 = delta_t[1]  # tdoa between mic 0 and 2
+    t3 = delta_t[2]  # tdoa between mic 1 and 2
+
+    start = time.time()
+    # solving for x and y using all 3 pair combinations
+
+    def system0(z):
+        x = z[0]
+        y = z[1]
+        f = np.zeros(2)
+        f[0] = sqrt((x-m0[0])**2 + (y-m0[1])**2) - sqrt((x-m1[0])**2 + (y-m1[1])**2) - (t1*v)
+        f[1] = sqrt((x-m0[0])**2 + (y-m0[1])**2) - sqrt((x-m2[0])**2 + (y-m2[1])**2) - (t2*v)
+        return f
+
+
+    a0 = fsolve(system0, [1, 1])
+    print(a0)
+    result.append(list(a0))
+
+
+    def system1(z):
+        x = z[0]
+        y = z[1]
+        f = np.zeros(2)
+        f[0] = sqrt((x-m0[0])**2.0 + (y-m0[1])**2.0) - sqrt((x-m1[0])**2.0 + (y-m1[1])**2.0) - (t1*v)
+        f[1] = sqrt((x-m1[0])**2.0 + (y-m1[1])**2.0) - sqrt((x-m2[0])**2.0 + (y-m2[1])**2.0) - (t3*v)
+        return f
+
+
+    a = fsolve(system1, [1, 1])
+    print(a)
+    result.append(list(a))
+
+    def system2(z):
+        x = z[0]
+        y = z[1]
+        f = np.zeros(2)
+        f[0] = sqrt((x-m0[0])**2.0 + (y-m0[1])**2.0) - sqrt((x-m2[0])**2.0 + (y-m2[1])**2.0) - (t2*v)
+        f[1] = sqrt((x-m1[0])**2.0 + (y-m1[1])**2.0) - sqrt((x-m2[0])**2.0 + (y-m2[1])**2.0) - (t3*v)
+        return f
+
+
+    a = fsolve(system2, [1, 1])
+    print(a)
+    result.append(list(a))
+
+    end = time.time()
+    print(end-start)
+    return result
+
+received_int = []
 
 @app.route('/post_json', methods=['POST'])
 def post_json():
     if request.method == 'POST':
         posted_data = request.get_json()
         print("Received JSON data:", posted_data)
-        received_strings.append(posted_data)
-        print(received_strings)
-        return "JSON data received and saved successfully!"
+        for value in posted_data.values():
+            received_int.append(value)
+        print("received:", received_int)
+
+        if len(received_int) == 3:
+            print("3 val:", received_int)
+            result = tdoa(COORD,V,received_int)
+            print(result)
+            return jsonify({"message": "Values extracted successfully!", "result": result[0]})
+        else:
+            return jsonify({"wait": "Waiting for more data"})
 
 @app.route('/get_received_json', methods=['GET'])
 def get_received_json():
-    return json.dumps(received_strings)
+    return json.dumps(received_int)
 
 if __name__ == '__main__':
-    app.run(host='172.30.75.165', port=5000)
+    app.run(host='192.168.137.249', port=5000)
--- test/test.py
+++ test/test.py
@@ -1,13 +1,34 @@
 import requests
 import json
+from math import sqrt
 
-url = 'http://172.30.75.165:5000/post_json'
+url = 'http://192.168.137.249:5000/post_json'
 
+def calculate_t():
+    m0 = [-3, 0]  # coordinates of mic 0 (x,y)
+    m1 = [3, 1]  # coordinates of mic 1 (x,y)
+    m2 = [0, 4]  # coordinates of mic 2 (x,y)
+
+    v = 1503.84  # velocity of sound in saltwater at 12 degrees C
+
+    # arrival times (pythagorean theorem for model with set point source as prompt)
+    xs = float(input('Source x: '))
+    ys = float(input('Source y: '))
+    a0 = sqrt((-3.0-xs)**2.0 + (0.0-ys)**2.0)/v  # arrival time of mic 0
+    a1 = sqrt((3.0-xs)**2.0 + (1.0-ys)**2.0)/v  # arrival time of mic 1
+    a2 = sqrt((0.0-xs)**2.0 + (4.0-ys)**2.0)/v  # arrival time of mic 2
+
+    # tdoa
+    t1 = a0 - a1  # tdoa between mic 0 and 1
+    t2 = a0 - a2  # tdoa between mic 0 and 2
+    t3 = a1 - a2  # tdoa between mic 1 and 2
+
+    return [t1,t2,t3]
+t = calculate_t()
 json_data = [
-    {"key1": "value1"},
-    {"key2": "value2"},
-    {"key3": "value3"},
-    {"key4": "value4"}
+    {"key1": t[0]},
+    {"key2": t[1]},
+    {"key3": t[2]},
 ]
 
 for data in json_data:
